bc42f12fa53322eae5b62d1034a494d075c84117
Osama Allam <osama.allam@tpay.me>
2020-03-22 21:16:05 +0200

    Add project files.

---
+++ ITI_EWD_DP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITI_EWD_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
