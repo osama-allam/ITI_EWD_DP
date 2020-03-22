ead31b6068b93281104ec9472d9bbd456b96acfd
Osama Allam <osama.allam@tpay.me>
2020-03-23 01:27:46 +0200

    [Iterator] simple word iterator

---
+++ Iterator.GenericImplementation/Iterator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace Iterator.GenericImplementation
+{
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        // Returns the current element
+        public abstract object Current();
+        
+        // Move forward to next element
+        public abstract bool MoveNext();
+        
+        // Rewinds the Iterator to the first element
+        public abstract void Reset();
+    }
+}
---
+++ Iterator.GenericImplementation/WordsCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator.GenericImplementation
+{
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+        
+        bool _direction = false;
+        
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+        
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+        
+        public void AddItem(string item)
+        {
+            _collection.Add(item);
+        }
+        
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, _direction);
+        }
+    }
+}
